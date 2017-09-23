470c175d0c7f7c18a5fb9cecdc8078b1fc584f4c
idea-PC\Shergat <cbellom@unal.edu.co>
2017-09-23 17:28:48 -0500

    search route style

--- src/pages/search-route/search-route.ts
+++ src/pages/search-route/search-route.ts
@@ -7,6 +7,12 @@ import { ResultRoutesPage } from '../result-routes/result-routes';
   templateUrl: 'search-route.html'
 })
 export class SearchRoutePage {
+  public points : any = [
+    {title: "from University to House", type:"Bicycle + Walk"},
+    {title: "from University to Work", type:"Walk"},
+    {title: "from Work to House", type:"Bicycle + Bus"},
+    {title: "from Work to Bar", type:"Bus"},
+  ];
 
   constructor(public navCtrl: NavController) {
   }
